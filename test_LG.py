b51db15cc50e338b6e4138b9a098126994d47c2b
RonZhu <dsfhe49854@gmail.com>
2019-10-23 17:46:58 +0800

    Make the nessary change to test smartphone raw image

--- test_LG.py
+++ test_LG.py
@@ -9,19 +9,10 @@ import numpy as np
 import rawpy
 import glob
 
-input_dir = './dataset/Sony/short/'
-gt_dir = './dataset/Sony/long/'
+input_dir = './dataset/LG2/'
 checkpoint_dir = './checkpoint/Sony/'
-result_dir = './result_Sony/'
+result_dir = './result_LG/'
 
-# get test IDs
-test_fns = glob.glob(gt_dir + '/1*.ARW')
-test_ids = [int(os.path.basename(test_fn)[0:5]) for test_fn in test_fns]
-
-DEBUG = 0
-if DEBUG == 1:
-    save_freq = 2
-    test_ids = test_ids[0:5]
 
 
 def lrelu(x):
@@ -103,73 +94,84 @@ def pack_raw(raw, black_level=512, raw_pattern=[[0, 1], [3, 2]]):
     return out
 
 
-sess = tf.Session()
-in_image = tf.placeholder(tf.float32, [None, None, None, 4])
-gt_image = tf.placeholder(tf.float32, [None, None, None, 3])
-out_image = network(in_image)
+with tf.Session() as sess:
+    in_image = tf.placeholder(tf.float32, [None, None, None, 4])
+    gt_image = tf.placeholder(tf.float32, [None, None, None, 3])
+    out_image = network(in_image)
 
-saver = tf.train.Saver()
-sess.run(tf.global_variables_initializer())
-ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-if ckpt:
-    print('loaded ' + ckpt.model_checkpoint_path)
-    saver.restore(sess, ckpt.model_checkpoint_path)
+    saver = tf.train.Saver()
+    sess.run(tf.global_variables_initializer())
+    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+    if ckpt:
+        print('loaded ' + ckpt.model_checkpoint_path)
+        saver.restore(sess, ckpt.model_checkpoint_path)
 
-if not os.path.isdir(result_dir + 'final/'):
-    os.makedirs(result_dir + 'final/')
+    if not os.path.isdir(result_dir + 'final/'):
+        os.makedirs(result_dir + 'final/')
+    raw_files = glob.glob(os.path.join(input_dir, '*.dng'))
 
-for test_id in test_ids:
-    # test the first image in each sequence
-    in_files = glob.glob(input_dir + '%05d_00*.ARW' % test_id)
-    for k in range(len(in_files)):
-        in_path = in_files[k]
+    for in_path in raw_files:
         in_fn = os.path.basename(in_path)
         print(in_fn)
-        gt_files = glob.glob(gt_dir + '%05d_00*.ARW' % test_id)
-        gt_path = gt_files[0]
-        gt_fn = os.path.basename(gt_path)
-        in_exposure = float(in_fn[9:-5])
-        gt_exposure = float(gt_fn[9:-5])
-        ratio = min(gt_exposure / in_exposure, 300)
+        ratio = min(100, 300)
 
         raw = rawpy.imread(in_path)
         blevel = raw.black_level_per_channel
         blevel = int(sum(blevel) / len(blevel))
+        raw_np = pack_raw(raw, black_level=blevel, raw_pattern=raw.raw_pattern)
         print('black level: ', blevel)
         print(raw.raw_pattern)
-        input_full = np.expand_dims(
-            pack_raw(raw, black_level=blevel, raw_pattern=raw.raw_pattern),
-            axis=0) * ratio
-
-        im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        # scale_full = np.expand_dims(np.float32(im/65535.0),axis = 0)*ratio
-        scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
 
         pypost = raw.postprocess()
+        output_size = [
+            pypost.shape[1] // 2,
+            pypost.shape[0] // 2,
+        ]
         scipy.misc.toimage(
             pypost
+        ).resize(
+            output_size
         ).save(
-            result_dir + 'final/%5d_00_post.png' % (test_id,)
+            os.path.join(result_dir, in_fn.replace('.dng', '.rawpy.png'))
         )
 
-        gt_raw = rawpy.imread(gt_path)
-        im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        gt_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+        campost = raw.postprocess(use_camera_wb=True, half_size=False,
+                                no_auto_bright=True, output_bps=8)
+        scipy.misc.toimage(
+            campost
+        ).resize(
+            output_size
+        ).save(
+            os.path.join(result_dir, in_fn.replace('.dng', '.cam.png'))
+        )
 
-        input_full = np.minimum(input_full, 1.0)
+        out_per_ratio = []
+        for ratio in [100, 200, 300]:
+            input_full = np.expand_dims(raw_np, axis=0) * ratio
+            input_full = np.minimum(input_full, 1.0)
 
-        output = sess.run(out_image, feed_dict={in_image: input_full})
-        output = np.minimum(np.maximum(output, 0), 1)
+            output = sess.run(out_image, feed_dict={in_image: input_full})
+            output = np.minimum(np.maximum(output, 0), 1)
 
-        output = output[0, :, :, :]
-        gt_full = gt_full[0, :, :, :]
-        scale_full = scale_full[0, :, :, :]
-        scale_full = scale_full * np.mean(gt_full) / np.mean(
-            scale_full)  # scale the low-light image to the same mean of the groundtruth
+            output = output[0, :, :, :]
+            output = np.flip(np.transpose(output, (1, 0, 2)), axis=1)
 
-        scipy.misc.toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
-            result_dir + 'final/%5d_00_%d_out.png' % (test_id, ratio))
-        scipy.misc.toimage(scale_full * 255, high=255, low=0, cmin=0, cmax=255).save(
-            result_dir + 'final/%5d_00_%d_scale.png' % (test_id, ratio))
-        scipy.misc.toimage(gt_full * 255, high=255, low=0, cmin=0, cmax=255).save(
-            result_dir + 'final/%5d_00_%d_gt.png' % (test_id, ratio))
+            scipy.misc.toimage(
+                output * 255, high=255, low=0, cmin=0, cmax=255
+            ).resize(
+                output_size
+            ).save(
+                os.path.join(result_dir, in_fn.replace('.dng', f'.{ratio}.png'))
+            )
+            out_per_ratio.append((output * 255).astype(np.uint8))
+            # scipy.misc.toimage(out_per_ratio[-1]).save(
+            #     os.path.join(result_dir, 'test.png')
+            # )
+            # import pdb; pdb.set_trace()
+        
+        concated = np.concatenate([campost, pypost] + out_per_ratio, axis=1)
+        concated2 = np.concatenate(out_per_ratio, axis=1)
+        ccat_size = [concated.shape[1] // 4, concated.shape[0] // 4]
+        scipy.misc.toimage(concated).resize(ccat_size).save(
+            os.path.join(result_dir, in_fn.replace('.dng', f'.concat.png'))
+        )
